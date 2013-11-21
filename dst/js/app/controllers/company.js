32707368ba4468d9c1b48f6a337b4de46591b69d
Ivan <ivanich86@gmail.com>
2013-11-21 17:56:52 +0200

    feat(project): v0.4.8

--- dst/js/app/controllers/company.js
+++ dst/js/app/controllers/company.js
@@ -20,7 +20,7 @@ define(['jquery',
             // Script collection for rjs optimization (bundle or debug)
             var workspaceWrapUrl = '';
             // 
-            workspaceWrapUrl = 'app/workspace-wrap-bundle-0.4.8';
+            workspaceWrapUrl = 'app/workspace-wrap-bundle-0.4.10';
             // 
 
             require([workspaceWrapUrl], function () {
