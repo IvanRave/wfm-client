a736e3ee50c6eceb327e83178ad2720a6cdda56a
Ivan <ivanich86@gmail.com>
2013-11-22 16:24:23 +0200

    feat(job type): add model

---
+++ src/js/app/models/job-type.js
@@ -0,0 +1,15 @@
+﻿define(['knockout'], function (ko) {
+    'use strict';
+
+    function JobType(data) {
+        var self = this;
+        data = data || {};
+
+        self.id = data.Id;
+        self.name = ko.observable(data.Name);
+        self.description = ko.observable(data.Description);
+        self.companyId = data.CompanyId;
+    }
+
+    return JobType;
+});
