ca6df74f3853646aa7ee12a8a4f4b9c3ded6d1ae
Ivan <ivanich86@gmail.com>
2014-01-05 21:11:00 +0200

    refactor(fileSpecService): change to plural words

--- src/js/services/file-spec.js
+++ src/js/services/file-spec.js
@@ -1,45 +1,28 @@
-﻿define(['jquery', 'services/datacontext', 'helpers/ajax-request'], function ($, datacontext, ajaxRequest) {
+﻿/** @module */
+define(['constants/stage-constants', 'helpers/ajax-request'], function (stageConstants, ajaxRequest) {
     'use strict';
 
-    var urls = {
-        company: function (idOfSection, idOfFileSpec) {
-            return '{{conf.requrl}}/api/company-sections/' + idOfSection + '/file-specs' + (idOfFileSpec ? ('/' + idOfFileSpec) : '');
-        },
-        wegion: function (idOfSection, idOfFileSpec) {
-            return '{{conf.requrl}}/api/region-sections/' + idOfSection + '/file-specs' + (idOfFileSpec ? ('/' + idOfFileSpec) : '');
-        },
-        /**
-        * Url of file spec of section of well field
-        * @param {string} idOfSection - Id of well field section (guid)
-        * @param {string} idOfFileSpec - Id of file specification (guid)
-        */
-        wield: function (idOfSection, idOfFileSpec) {
-            return '{{conf.requrl}}/api/field-sections/' + idOfSection + '/file-specs' + (idOfFileSpec ? ('/' + idOfFileSpec) : '');
-        },
-        wroup: function (idOfSection, idOfFileSpec) {
-            return '{{conf.requrl}}/api/group-sections/' + idOfSection + '/file-specs' + (idOfFileSpec ? ('/' + idOfFileSpec) : '');
-        },
-        /**
-        * Url of file spec of section of well
-        * @param {string} idOfSection - Id of well section (guid)
-        * @param {string) idOfFileSpec - Id of file spec (guid)
-        */
-        well: function (idOfSection, idOfFileSpec) {
-            return '{{conf.requrl}}/api/well-sections/' + idOfSection + '/file-specs' + (idOfFileSpec ? ('/' + idOfFileSpec) : '');
-        }
+    /** Generate url */
+    var url = function (typeOfStage, idOfSection, idOfFileSpec) {
+        var stagePlural = stageConstants[typeOfStage].plural;
+        return '{{conf.requrl}}/api/' + stagePlural + '/sections/' + idOfSection + '/file-specs' + (idOfFileSpec ? ('/' + idOfFileSpec) : '');
     };
 
+    /**
+    * File spec service
+    * @constructor
+    */
     var exports = {
         // Get url for posting new file
         getUrl: function (typeOfStage, idOfSection, idOfFileSpec) {
-            return urls[typeOfStage](idOfSection, idOfFileSpec);
+            return url(typeOfStage, idOfSection, idOfFileSpec);
         },
         get: function (typeOfStage, idOfSection) {
-            return ajaxRequest('GET', urls[typeOfStage](idOfSection));
+            return ajaxRequest('GET', url(typeOfStage, idOfSection));
         },
         // Delete few files in one request
         deleteArray: function (typeOfStage, idOfSection, listOfFileSpec) {
-            return ajaxRequest('DELETE', urls[typeOfStage](idOfSection), listOfFileSpec);
+            return ajaxRequest('DELETE', url(typeOfStage, idOfSection), listOfFileSpec);
         }
     };
 
