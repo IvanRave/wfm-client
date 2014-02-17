c4d5b933a5331520cad0de548e74a9294af76639
Ivan <ivanich86@gmail.com>
2014-02-18 00:14:05 +0200

    feat(procentBorder): model and data service
    procent border for wfm parameters of well
    
    time: 0h:15

---
+++ src/js/models/procent-border.js
@@ -0,0 +1,13 @@
+﻿/** @module */
+define([], function () {
+	'use strict';
+
+	/**
+	 * Model: procent border for a WFM parameter in every well
+	 * @constructor
+	 */
+	var exports = function () {
+	};
+
+	return exports;
+});
---
+++ src/js/services/procent-border.js
@@ -0,0 +1,26 @@
+﻿/** @module */
+define(['helpers/ajax-request'], function (ajaxRequest) {
+	'use strict';
+
+  var url = function (idOfWell, idOfWfmParameter) {
+     return '{{conf.requrl}}/api/wells/' + idOfWell +'/procent-borders' + (idOfWfmParameter ? ('/' + idOfWfmParameter) : '');
+  };
+  
+	/**
+	 * Model: procent border service
+	 * @constructor
+	 */
+	var exports = {
+    get: function(idOfWell){
+      return ajaxRequest('GET', url(idOfWell));
+    },
+    insertOrUpdate: function(idOfWell){
+      return ajaxRequest('POST', url(idOfWell));
+    },
+    remove: function(idOfWell, idOfWfmParameter){
+      return ajaxRequest('DELETE', url(idOfWell, idOfWfmParameter));
+    }
+  };
+
+	return exports;
+});
