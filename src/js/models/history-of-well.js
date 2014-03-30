8f25e741ccd0f0d379aa11b95f6ae4520e4a6ed6
Ivan <ivanich86@gmail.com>
2014-03-30 16:41:30 +0400

    refactor(wellHistory): optimize methods
    move to the prototype
    
    time: 0h:0

--- src/js/models/history-of-well.js
+++ src/js/models/history-of-well.js
@@ -30,7 +30,6 @@ define(['jquery',
 	 * @constructor
 	 */
 	var exports = function (data, well) {
-		var ths = this;
 		data = data || {};
 
 		this.getWell = function () {
@@ -46,100 +45,45 @@ define(['jquery',
 		this.wellId = data.WellId;
 
 		this.WfmImages = ko.observableArray();
-    
-    /**
-    * A list of history files
-    * @type {Array.<module:models/file-spec>}
-    */
+
+		/**
+		 * A list of history files
+		 * @type {Array.<module:models/file-spec>}
+		 */
 		this.WellHistoryFiles = ko.observableArray(importWellHistoryFiles(data.WellHistoryFiles));
+
+		/**
+		 * A list of job types of this company
+		 * @type {module: models/job-type}
+		 */
+		this.listOfJobTypeOfCompany = ko.computed({
+				read : this.calcListOfJobTypeOfCompany,
+				deferEvaluation : true,
+				owner : this
+			});
 
 		// Load job type id
 		// Extract from root.companyJobTypeList by id
 		// Computed
 		this.jobType = ko.computed({
-				read : function () {
-					var tmpJobTypeId = ko.unwrap(ths.jobTypeId);
-					if (tmpJobTypeId) {
-						var companyJobTypeList = ko.unwrap(ths.getWell().getWellGroup().getWellField().getWellRegion().getCompany().jobTypeList);
-						return appHelper.getElementByPropertyValue(companyJobTypeList, 'id', tmpJobTypeId);
-					}
-				},
-				deferEvaluation : true
+				read : this.calcJobTypeById,
+				deferEvaluation : true,
+				owner : this
 			});
 
+		/**
+		 * Whether the end time block is visible
+		 * @type {boolean}
+		 */
 		this.isVisibleEndUnixTime = ko.computed({
-				read : function () {
-					return ko.unwrap(ths.startUnixTime) !== ko.unwrap(ths.endUnixTime);
-				},
-				deferEvaluation : true
+				read : this.calcIsVisEndUnixTime,
+				deferEvaluation : true,
+				owner : this
 			});
 
-		this.putWellHistory = function () {
-			var wellHistoryData = {
-				id : ko.unwrap(ths.id),
-				startUnixTime : ko.unwrap(ths.startUnixTime),
-				endUnixTime : ko.unwrap(ths.endUnixTime),
-				wellId : ko.unwrap(ths.wellId),
-				historyText : ko.unwrap(ths.historyText),
-				jobTypeId : ko.unwrap(ths.jobTypeId)
-			};
-
-			datacontext.putWellHistory(wellHistoryData);
-		};
-
-		this.startUnixTime.subscribe(ths.putWellHistory);
-		this.endUnixTime.subscribe(ths.putWellHistory);
-		this.jobTypeId.subscribe(ths.putWellHistory);
-
-		this.removeWfmImage = function (itemForDelete) {
-			if (confirm('{{capitalizeFirst lang.confirmToDelete}}?')) {
-				imageOfHistoryOfWellService.remove(ths.id, itemForDelete.id).done(function () {
-					ths.WfmImages.remove(itemForDelete);
-				});
-			}
-		};
-
-		/** Remove file spec of history of well */
-		this.removeFileSpecOfHistoryOfWell = function (fileSpecOfHistoryOfWellToRemove) {
-			if (confirm('{{capitalizeFirst lang.confirmToDelete}} this file?')) {
-				fileSpecOfHistoryOfWellService.remove(ths.id, fileSpecOfHistoryOfWellToRemove.idOfFileSpec).done(function () {
-					ths.WellHistoryFiles.remove(fileSpecOfHistoryOfWellToRemove);
-				});
-			}
-		};
-
-		/**
-		 * Create file spec of history of well from well section
-		 */
-		this.postFileSpecOfHistoryOfWell = function (tmpIdOfFileSpec, scsCallback, errCallback) {
-			fileSpecOfHistoryOfWellService.post(ths.id, {
-				idOfFileSpec : tmpIdOfFileSpec,
-				idOfHistoryOfWell : ths.id,
-				description : ''
-			}).done(function (res) {
-				ths.WellHistoryFiles.push(new FileSpecOfHistoryOfWell(res));
-				// Hide window
-				scsCallback();
-			}).fail(errCallback);
-		};
-
-		/**
-		 * Create cropped well history image from file spec
-		 */
-		this.postImageOfHistoryOfWell = function (tmpIdOfFileSpec, coords, scsCallback) {
-			imageOfHistoryOfWellService.post(ths.id, {
-				IdOfFileSpec : tmpIdOfFileSpec,
-				X1 : coords[0],
-				X2 : coords[2],
-				Y1 : coords[1],
-				Y2 : coords[3],
-				CropXUnits : 0,
-				CropYUnits : 0
-			}).done(function (res) {
-				ths.WfmImages.push(new WfmImage(res));
-				scsCallback();
-			});
-		};
+		this.startUnixTime.subscribe(this.putWellHistory, this);
+		this.endUnixTime.subscribe(this.putWellHistory, this);
+		this.jobTypeId.subscribe(this.putWellHistory, this);
 
 		if (data.WfmImagesDto) {
 			this.WfmImages(importWfmImagesDto(data.WfmImagesDto));
@@ -149,6 +93,106 @@ define(['jquery',
 		////if (data.JobTypeDto) {
 		////    ths.jobType(new JobType(data.JobTypeDto));
 		////}
+	};
+
+	/**
+	 * Calc a list with job types
+	 * @private
+	 * @returns {Array.<module:models/job-type>}
+	 */
+	exports.prototype.calcListOfJobTypeOfCompany = function () {
+		return ko.unwrap(this.getWell().getWellGroup().getWellField().getWellRegion().getCompany().jobTypeList);
+	};
+
+	/**
+	 * Calc a job type using id
+	 * @return {module:models/job-type
+	 */
+	exports.prototype.calcJobTypeById = function () {
+		var tmpJobTypeId = ko.unwrap(this.jobTypeId);
+		if (tmpJobTypeId) {
+			var companyJobTypeList = ko.unwrap(this.listOfJobTypeOfCompany);
+			return appHelper.getElementByPropertyValue(companyJobTypeList, 'id', tmpJobTypeId);
+		}
+	};
+
+	/**
+	 * Calc visibility of the end time block
+	 * @return {boolean}
+	 */
+	exports.prototype.calcIsVisEndUnixTime = function () {
+		return ko.unwrap(this.startUnixTime) !== ko.unwrap(this.endUnixTime);
+	};
+
+	/**
+	 * Save a history
+	 */
+	exports.prototype.putWellHistory = function () {
+		datacontext.putWellHistory({
+			id : ko.unwrap(this.id),
+			startUnixTime : ko.unwrap(this.startUnixTime),
+			endUnixTime : ko.unwrap(this.endUnixTime),
+			wellId : ko.unwrap(this.wellId),
+			historyText : ko.unwrap(this.historyText),
+			jobTypeId : ko.unwrap(this.jobTypeId)
+		});
+	};
+
+	/**
+	 * Remove an image from history
+	 */
+	exports.prototype.removeWfmImage = function (itemForDelete) {
+		if (confirm('{{capitalizeFirst lang.confirmToDelete}}?')) {
+			var ths = this;
+			imageOfHistoryOfWellService.remove(this.id, itemForDelete.id).done(function () {
+				ths.WfmImages.remove(itemForDelete);
+			});
+		}
+	};
+
+	/** Remove file spec of history of well */
+	exports.prototype.removeFileSpecOfHistoryOfWell = function (fileSpecOfHistoryOfWellToRemove) {
+		if (confirm('{{capitalizeFirst lang.confirmToDelete}} this file?')) {
+			var ths = this;
+			fileSpecOfHistoryOfWellService.remove(ths.id, fileSpecOfHistoryOfWellToRemove.idOfFileSpec).done(function () {
+				ths.WellHistoryFiles.remove(fileSpecOfHistoryOfWellToRemove);
+			});
+		}
+	};
+
+	/**
+	 * Create file spec of history of well from well section
+	 */
+	exports.prototype.postFileSpecOfHistoryOfWell = function (tmpIdOfFileSpec, scsCallback, errCallback) {
+		var ths = this;
+		fileSpecOfHistoryOfWellService.post(ths.id, {
+			idOfFileSpec : tmpIdOfFileSpec,
+			idOfHistoryOfWell : ths.id,
+			description : ''
+		}).done(function (res) {
+			ths.WellHistoryFiles.push(new FileSpecOfHistoryOfWell(res));
+			// Hide window
+			scsCallback();
+		}).fail(errCallback);
+	};
+
+	/**
+	 * Create cropped well history image from file spec
+	 */
+	this.postImageOfHistoryOfWell = function (tmpIdOfFileSpec, coords, scsCallback) {
+    var ths = this;
+		imageOfHistoryOfWellService.post(ths.id, {
+			IdOfFileSpec : tmpIdOfFileSpec,
+			X1 : coords[0],
+			X2 : coords[2],
+			Y1 : coords[1],
+			Y2 : coords[3],
+			CropXUnits : 0,
+			CropYUnits : 0
+		}).done(function (res) {
+			ths.WfmImages.push(new WfmImage(res));
+			scsCallback();
+		});
 	};
 
 	return exports;
